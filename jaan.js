b5029270adc43e1978bf29b72ba6039181b040bc
mehedihasan <mehedihasan645356@gmail.com>
2024-08-29 20:36:26 +0600

    task

---
+++ jaan.js
@@ -0,0 +1,24 @@
+// var car = "red-car"
+// car ="green-car";
+// let car = "red-car";
+// const car = "red"
+// console.log(car);
+
+// arithmatic operator --- modulus (%) for finding the remainder
+// var num1 = 20 ;
+// var num2 = 12
+//  var remainder = num1 % num2 ;
+
+//  console.log(remainder);
+
+// var price= 35;
+// price = price + 10;
+//  price += 10 ;
+// console.log(price);
+// concatination
+// var cow= "cow = 100";
+// var goat = "goat = " + 20;
+// var all = cow + goat ;
+// console.log(all);
+
+
---
+++ task.js
@@ -0,0 +1,46 @@
+// task-1
+var orange1kg = 300 ;
+var apple1gk = 400 ;
+
+  apple2gk = apple1gk * 2
+  orange2kg = orange1kg * 2
+
+var buytotal = orange2kg + apple2gk ;
+
+var totalMoney = 1500 ;
+
+console.log (totalMoney - buytotal );
+
+// task--2
+// ques : Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student.
+var Mathematics = 70 ;
+var Biology = 80 ;
+var Chemistry = 97.56 ;
+var Physics = 60 ;
+var bangla = 50 ;
+
+var totalnum = Mathematics + Biology + Chemistry + Physics +bangla ;
+
+var average = totalnum / 5 ;
+
+console.log(average.toFixed(2));
+// task -3
+// You task is to divide the given number by 5 and show the remainder as the output.
+
+console.log(119 % 5);
+
+// task--4
+//  What will be the result of the following codes:
+
+// var a = isNaN(‘11’); becouse of the number inside the quate thats why its a string and is not a num and its true
+
+// var a = isNaN(2-10); this statment is false becouse the number-8 number.
+//  but it can  be true if we write this statement like var a = !isNan(2-8); it can change the statement
+// var a = isNaN (2-8);
+// var a = !isNaN (2-8);
+
+console.log(a);
+
+
+
+
