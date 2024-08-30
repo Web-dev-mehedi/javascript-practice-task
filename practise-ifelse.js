1e5acd447f725b1a88038e6e67c51e252b18aa24
mehedihasan <mehedihasan645356@gmail.com>
2024-08-31 01:59:10 +0600

    array

---
+++ practise-ifelse.js
@@ -0,0 +1,21 @@
+// positive -negetive number check
+let number = -20;
+if (number > 0){
+  console.log("this number is positive")
+}
+else {
+    console.log("this number is nagetive")
+};
+
+// check the number is even or add
+let isnumber = prompt("enter your number :");
+if(isnumber % 2 === 0 ){
+    console.log("জোড়");
+}
+else {
+    console.log(" বেজোড় ")
+}
+
+// Exercise #3 — Determine the Greater Number
+
+// var num1 =
